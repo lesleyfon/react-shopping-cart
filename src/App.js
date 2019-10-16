6ccaafcb585705c77312c91037e79501077e672f
lesley <lesleybanadzem2017@gmail.com>
2019-10-16 16:34:26 -0700

    Imported the productContext and wrapped the app.js jsx with the Provider component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './context/ProductContext'
 
 // Components
 import Navigation from './components/Navigation';
@@ -10,34 +11,45 @@ import ShoppingCart from './components/ShoppingCart';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
-
+	//
+	
 	useEffect(()=>{}, [cart])
 	const addItem = item => {
 		// add the given item to the cart
 		setCart([...cart, item])
 	};
 
+	const productsContextObject = {
+			products: products,
+			cart: cart,
+			setCart: setCart,
+			addItem : addItem
+		}
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider>
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					render={() => (
+						<Products
+							products={products}
+							addItem={addItem}
+						/>
+					)}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</div>
+		</ProductContext.Provider>
+
 	);
 }
 
